0a5844b283b4d07aebe2403d440c6d695c56e0c6
Phyllis Wong <wong.phyllism@gmail.com>
2017-10-02 16:01:58 -0700

    delegates practice lesson

---
+++ delegatesPractice/ReceivingVC.temp_caseinsensitive_rename.swift
@@ -0,0 +1,25 @@
+//
+//  ReceivingVC.swift
+//  delegatesPractice
+//
+//  Created by djchai on 10/2/17.
+//  Copyright © 2017 djchai. All rights reserved.
+//
+
+import UIKit
+
+class ReceivingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ delegatesPractice/SendingVC.swift
@@ -0,0 +1,20 @@
+//
+//  SendingVC.swift
+//  delegatesPractice
+//
+//  Created by djchai on 10/2/17.
+//  Copyright © 2017 djchai. All rights reserved.
+//
+
+import UIKit
+
+class SendingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
