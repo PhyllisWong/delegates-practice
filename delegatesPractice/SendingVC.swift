71f547fa39accee37c0aedb8b407bbd0f662e7b8
Phyllis Wong <wong.phyllism@gmail.com>
2017-10-02 18:01:05 -0700

    added UI and delegate

--- delegatesPractice/SendingVC.swift
+++ delegatesPractice/SendingVC.swift
@@ -8,12 +8,26 @@
 
 import UIKit
 
+protocol DataSentDelegate {
+    func userDidEnterData(data: String)
+}
+
 class SendingVC: UIViewController {
 
+    var delegate: DataSentDelegate? = nil
+    
+    @IBOutlet weak var dataEntryTextField: UITextField!
+    
+    @IBAction func didPressSend(_ sender: Any) {
+        if delegate != nil {
+            guard let data = dataEntryTextField.text else {return}
+            delegate?.userDidEnterData(data: data)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
 
